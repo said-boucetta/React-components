e0e52131df3197d8958a72f904a8f52fe1f57157
Boucetta_Said <said19022018@gmail.com>
2021-04-28 13:29:34 +0100

    first message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todoArr, setTodoArr] = useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <form>
+        <input
+          onChange={(e) => {
+            setTodo(e.target.value);
+          }}
+          value={todo} //meaning?
+        ></input>
+        <button
+          onClick={(e) => {
+            e.preventDefault(); //meaning?
+            setTodoArr([
+              ...todoArr,
+              {
+                id: uuidv4(),
+                task: todo,
+              },
+            ]);
+            setTodo("");
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          ok
+        </button>
+      </form>
+      <ul>
+        {todoArr.map((todoItem) => (
+          <li key={todoItem.id}>
+            {todoItem.task}
+            <button
+              onClick={() =>
+                setTodoArr(todoArr.filter((elem) => elem.id !== todoItem.id))
+              }
+            >
+              X
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
